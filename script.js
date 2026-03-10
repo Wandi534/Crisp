83a65c7f1797e77e986b15e9debe3e876e892859
Wandi534 <luyandamahlangu16@gmail.com>
2026-03-10 20:02:06 +0200

    fix: correct navbar-link class and add mobile nav toggle

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ const addEventOnElements = function (elements, eventType, callback) {
     }
 }
 
+/*** MOBILE NAV*/
+const navbar = document.querySelector("[data-navbar]");
+const navbarTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navbarlinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+const togglerNav= function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+}
+
+addEventOnElements(navbarTogglers, "click", togglerNav);
+addEventOnElements(navbarlinks, "click", togglerNav);
+  
+const closeNav = function () {
+    navbar.classList.remove("active");
+    overlay.classList.remove("active");
+}
+
+addEventOnElements(navbarlinks, "click", closeNav);
