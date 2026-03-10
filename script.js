e20952a2afd86b6e2b8244e48837b023b5c9eaa2
Wandi534 <luyandamahlangu16@gmail.com>
2026-03-10 19:18:57 +0200

    Add event listeners

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/***add event listener on multiple elements ***/
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
